09b69b7a053efbbecff9c13012e879a1dedf9603
0xj3lly <0xj3lly@gmail.com>
2020-07-27 11:27:17 +0100

    removed region lock from prowler

--- aws-audit/audit.sh
+++ aws-audit/audit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 scout --provider aws --profile $1 --report-dir /output --report-name $1 
-/prowler/prowler -q -p $1 -f "eu-west-1 eu-west-2 us-east-1" |& tee /output/$1.prowler
+/prowler/prowler -q -p $1 |& tee /output/$1.prowler
